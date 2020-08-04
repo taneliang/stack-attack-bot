dfe622ef2039d9e4571d197363a1d6c523a9914f
E-Liang Tan <eliang@eliangtan.com>
2020-08-04 10:24:44 +0800

    Fix lint, update tsconfig

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,64 +1,53 @@
-// You can import your modules
-// import index from '../src/index'
-
-import nock from 'nock'
+import nock from "nock";
 // Requiring our app implementation
-import myProbotApp from '../src'
-import { Probot } from 'probot'
+import myProbotApp from "../src";
+import { Probot } from "probot";
 // Requiring our fixtures
-import payload from './fixtures/issues.opened.json'
-const issueCreatedBody = { body: 'Thanks for opening this issue!' }
-const fs = require('fs')
-const path = require('path')
+import payload from "./fixtures/issues.opened.json";
+const issueCreatedBody = { body: "Thanks for opening this issue!" };
+import fs from "fs";
+import path from "path";
 
-describe('My Probot app', () => {
-  let probot: any
-  let mockCert: string
+describe("My Probot app", () => {
+  let probot: Probot;
+  let mockCert: string;
 
-  beforeAll((done: Function) => {
-    fs.readFile(path.join(__dirname, 'fixtures/mock-cert.pem'), (err: Error, cert: string) => {
-      if (err) return done(err)
-      mockCert = cert
-      done()
-    })
-  })
+  beforeAll((done) => {
+    fs.readFile(path.join(__dirname, "fixtures/mock-cert.pem"), (err, cert) => {
+      if (err) return done(err);
+      mockCert = cert.toString();
+      done();
+    });
+  });
 
   beforeEach(() => {
-    nock.disableNetConnect()
-    probot = new Probot({ id: 123, cert: mockCert })
+    nock.disableNetConnect();
+    probot = new Probot({ id: 123, cert: mockCert });
     // Load our app into probot
-    probot.load(myProbotApp)
-  })
+    probot.load(myProbotApp);
+  });
 
-  test('creates a comment when an issue is opened', async (done) => {
+  // eslint-disable-next-line jest/no-test-callback
+  test("creates a comment when an issue is opened", async (done) => {
     // Test that we correctly return a test token
-    nock('https://api.github.com')
-      .post('/app/installations/2/access_tokens')
-      .reply(200, { token: 'test' })
+    nock("https://api.github.com")
+      .post("/app/installations/2/access_tokens")
+      .reply(200, { token: "test" });
 
     // Test that a comment is posted
-    nock('https://api.github.com')
-      .post('/repos/hiimbex/testing-things/issues/1/comments', (body: any) => {
-        done(expect(body).toMatchObject(issueCreatedBody))
-        return true
+    nock("https://api.github.com")
+      .post("/repos/hiimbex/testing-things/issues/1/comments", (body) => {
+        done(expect(body).toMatchObject(issueCreatedBody));
+        return true;
       })
-      .reply(200)
+      .reply(200);
 
     // Receive a webhook event
-    await probot.receive({ name: 'issues', payload })
-  })
+    await probot.receive({ id: "1234", name: "issues", payload });
+  });
 
   afterEach(() => {
-    nock.cleanAll()
-    nock.enableNetConnect()
-  })
-})
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
-
-// For more information about using TypeScript in your tests, Jest recommends:
-// https://github.com/kulshekhar/ts-jest
-
-// For more information about testing with Nock see:
-// https://github.com/nock/nock
+    nock.cleanAll();
+    nock.enableNetConnect();
+  });
+});
